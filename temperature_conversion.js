f3bf39cd01c0f9a886ee814d3de4f26270815028
johnny lamb <agnusjack@gmail.com>
2015-07-12 19:02:07 -0600

    temp converter after edit

--- temperature_conversion.js
+++ temperature_conversion.js
@@ -1,7 +1,8 @@
-var temp = prompt("give me a temperature in Celsius to convert to fahrenheit!")
+var temp = prompt("give me a temperature in Celsius to convert to fahrenheit!");
+
+
 
 
 
 console.log("Your temperature converts to ");
 console.log(temp * 9/5 +32);
-aler
